ab632f982087fec655c7c335ce08a11bd58b078a
Bhargav reddy <141399877+bhargavreddy2003@users.noreply.github.com>
2025-08-29 20:06:24 +0530

    Time: 4 ms (76.92%) | Memory: 12.4 MB (85.00%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class Solution(object):
+    def bfs(self, grid, q, fresh):
+        rows, cols = len(grid), len(grid[0])
+        time = 0
+
+        while q:
+            r, c, t = q.popleft()
+            time = max(time, t)   
+            for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    fresh -= 1
+                    q.append((nr, nc, t + 1))
+        return -1 if fresh > 0 else time
+
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        rows, cols = len(grid), len(grid[0])
+        q = deque()
+        fresh = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r, c, 0))  
+                elif grid[r][c] == 1:
+                    fresh += 1
+
+    
+        return self.bfs(grid, q, fresh)
